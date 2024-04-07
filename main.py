8ed4e0a7916fa7e5761e56bcfe48c5577eda33cb
Bill Weiss <github@houdini.spum.us>
2024-04-06 23:27:45 -0600

    Works, pubsub isn't going to get there

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 import asyncio
 import codecs
 import os
+import pprint
 import subprocess
 
 from dotenv import load_dotenv
 from twitchAPI.chat import Chat, EventData, ChatMessage, ChatSub, ChatCommand
 from twitchAPI.oauth import UserAuthenticator, UserAuthenticationStorageHelper
+#from twitchAPI.pubsub import PubSub
 from twitchAPI.twitch import Twitch
 from twitchAPI.type import AuthScope, ChatEvent
 
@@ -15,9 +17,16 @@ load_dotenv()
 
 TWITCH_APP_ID     = os.getenv("TWITCH_APP_ID")
 TWITCH_APP_SECRET = os.getenv("TWITCH_APP_SECRET")
-TWITCH_USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT, AuthScope.WHISPERS_READ]
+TWITCH_USER_SCOPE = [
+        AuthScope.CHAT_READ,
+        AuthScope.CHAT_EDIT,
+        AuthScope.WHISPERS_READ,
+        AuthScope.CHANNEL_READ_REDEMPTIONS,
+    ]
 TARGET_CHANNEL    = 'syntax976'
-ALLOWED_USERS     = ['billweiss']
+ALLOWED_USERS     = ['billweiss', 'beardbyte']
+# this is generated from some magic fucking site. Why, API, why.
+CHANNEL_ID        = 198813553
 
 # this will be called when the event READY is triggered, which will be on bot start
 async def on_ready(ready_event: EventData):
@@ -68,6 +77,10 @@ COMMANDS = {
         'reply': reply_command,
         'asdf': asdf_command,
         }
+#
+#async def points_listener(uuid, data):
+#    print(f'Callback for pubsub UUID {uuid}')
+#    pprint.pp(data)
 
 # this is where we set up the bot
 async def run():
@@ -79,7 +92,7 @@ async def run():
     # create chat instance
     chat = await Chat(twitch)
 
-    # register the handlers for the events you want
+#    pubsub = PubSub(twitch)
 
     # listen to when the bot is done starting up and ready to join channels
     chat.register_event(ChatEvent.READY, on_ready)
@@ -93,15 +106,19 @@ async def run():
     for command in COMMANDS.items():
         chat.register_command(command[0], command[1])
 
+#    uuid = await pubsub.listen_channel_points(CHANNEL_ID, points_listener)
+#    print(f'UUID from starting pubsub: {uuid}')
 
     # we are done with our setup, lets start this bot up!
     chat.start()
+#    pubsub.start()
 
     # lets run till we press enter in the console
     try:
-        input('press ENTER to stop\\n')
+        input('press ENTER to stop\n')
     finally:
         # now we can close the chat bot and the twitch api client
+#        pubsub.stop()
         chat.stop()
         await twitch.close()
 
